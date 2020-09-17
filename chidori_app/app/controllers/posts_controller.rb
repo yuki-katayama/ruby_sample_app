39a32f5a5da0421da9cc3b6857e0f6f5bef0881b
yuki-katayama <katayu810@gmail.com>
2020-09-17 21:50:12 +0900

    Add error cases

--- chidori_app/app/controllers/posts_controller.rb
+++ chidori_app/app/controllers/posts_controller.rb
@@ -6,24 +6,36 @@ class PostsController < ApplicationController
     @post = Post.find_by(id:params[:id])
   end
   def new
+    @post = Post.new
   end
   def create
-    @post = Post.new(content:params[:content])
-    @post.save
-    redirect_to('/posts/index')
+    @post = Post.new(content: params[:content])
+    if @post.save
+      flash[:notice] = "投稿を作成しましした to Chidori"
+      redirect_to('/posts/index')
+    else
+      render('posts/new')
+    end
   end
+
   def edit
     @post = Post.find_by(id:params[:id])
   end
   def update
     @post = Post.find_by(id:params[:id])
     @post.content = params[:content]
-    @post.save
-    redirect_to("/posts/index")
+
+    if @post.save
+      flash[:notice] = "編集内容を保存しました by Chidori"
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
+    end
   end
   def delete
     @post = Post.find_by(id:params[:id])
     @post.destroy
+    flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
   end
 
--- chidori_app/app/models/post.rb
+++ chidori_app/app/models/post.rb
@@ -1,4 +1,3 @@
 class Post < ApplicationRecord
-    validates :content, {presence: true}
-    validates :content, {length: {maximum: 140}}
+    validates :content, {presence: true, length: {maximum: 140}}
 end
