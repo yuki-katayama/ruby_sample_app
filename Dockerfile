f8af65e884885aad23e52ee3b56283c4c664d4c9
yuki-katayama <katayu810@gmail.com>
2020-09-19 23:52:52 +0900

    add .gitignore & setting

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM ruby:2.5
+RUN apt-get update -qq && apt-get install -y nodejs postgresql-client
+RUN mkdir /myapp
+WORKDIR /myapp
+COPY Gemfile /myapp/Gemfile
+COPY Gemfile.lock /myapp/Gemfile.lock
+RUN bundle install
+COPY . /myapp
+RUN apt-get install -y vim
+# railsを追加した場合 RUN sudo gem install rails
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'home/top'
+  get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
+  get 'posts/:id' => 'posts#show'
+  get '/' => "home#top"
+  post 'posts/create' => 'posts#create'
+  get  'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/delete' => 'posts#delete'
+  get 'users/index' => 'users#index'
+  get 'about' => "home#about"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
