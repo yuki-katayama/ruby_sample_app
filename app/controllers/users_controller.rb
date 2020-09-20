b0ab26bbf9a155c5f86ec23984b978135e20a713
yuki-katayama <katayu810@gmail.com>
2020-09-20 17:49:03 +0900

    Create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,40 @@ class UsersController < ApplicationController
     def index
         @users = User.all
     end
+    def show
+        @user = User.find_by(id: params[:id])
+        # redirect_to("users/#{@user.id}")
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        # @user = User.new
+        # @user.name = params[:name]
+        # @user.email = params[:email]
+        # @user.save
+        @user = User.new(name: params[:name], email: params[:email])
+        if @user.save
+            flash[:notice] = "aa"
+            redirect_to("/users/#{@user.id}")
+        else
+            render("users/new")
+        end
+    end
+    def edit
+        @user = User.find_by(id: params[:id])
+    end
+    def update
+        @user = User.find_by(id: params[:id])
+        @user.name = params[:name]
+        @user.email = params[:email]
+        if @user.save
+            flash[:notice] = "ユーザー情報を編集しました"
+            redirect_to("/users/#{@user.id}")
+        else
+            render("users/edit")
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/top'
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
   get 'posts/:id' => 'posts#show'
@@ -8,7 +7,15 @@ Rails.application.routes.draw do
   get  'posts/:id/edit' => 'posts#edit'
   post 'posts/:id/update' => 'posts#update'
   post 'posts/:id/delete' => 'posts#delete'
+
   get 'users/index' => 'users#index'
+  get 'signup' => 'users#new'
+  post 'users/create' => 'users#create'
+  post 'users/:id/update' => 'users#update'
+  get 'users/:id/edit' => 'users#edit'
+  get 'users/:id' => 'users#show'
+
+  get 'home/top'
   get 'about' => "home#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
