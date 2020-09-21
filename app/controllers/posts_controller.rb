8f02754139c7913e66a0464a9f163ca3f1cd7d0c
yuki-katayama <katayu810@gmail.com>
2020-09-22 01:27:23 +0900

    Add authenticate setting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
+
     def index
       @posts = Post.all.order(created_at: :desc)
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user, {only: [:index, :show, :update, :edit]}
+    before_action :forbid_login_user, {only: [:new, :create, :login, :login_form]}
+    before_action :ensure_correct_user, {only: [:edit, :update]}
+
+
     def index
         @users = User.all
     end
@@ -26,6 +31,7 @@ class UsersController < ApplicationController
     def edit
         @user = User.find_by(id: params[:id])
     end
+
     def update
         @user = User.find_by(id: params[:id])
         @user.name = params[:name]
@@ -42,8 +48,10 @@ class UsersController < ApplicationController
             render("users/edit")
         end
     end
+
     def login_form
     end
+
     def login
         @user = User.find_by(email: params[:email], password: params[:password])
         if @user
@@ -62,5 +70,12 @@ class UsersController < ApplicationController
         session[:user_id] = nil
         flash[:notice] = "ログアウトしました"
         redirect_to('/login')
+    end
+
+    def ensure_correct_user
+        if @current_user.id != params[:id].to_i
+            flash[:notice] = "権限がありません"
+            redirect_to("/posts/index")
+        end
     end
 end
