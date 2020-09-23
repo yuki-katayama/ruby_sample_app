acffd19dfee492d7c42ca6d005f2152f271258dc
yuki-katayama <katayu810@gmail.com>
2020-09-23 17:42:43 +0900

    connect user and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,19 @@
 class PostsController < ApplicationController
     before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
+    before_action :ensure_correct_user, {only: [:delete, :edit, :update]}
 
     def index
       @posts = Post.all.order(created_at: :desc)
     end
     def show
       @post = Post.find_by(id:params[:id])
+      @user = @post.user
     end
     def new
       @post = Post.new
     end
     def create
-      @post = Post.new(content: params[:content])
+      @post = Post.new(content: params[:content], user_id: @current_user.id)
       if @post.save
         flash[:notice] = "投稿を作成しましした to Chidori"
         redirect_to('/posts/index')
@@ -40,6 +42,12 @@ class PostsController < ApplicationController
       flash[:notice] = "投稿を削除しました"
       redirect_to("/posts/index")
     end
-  
+
+    def ensure_correct_user
+      if @current_user.id != @post.user_id
+        flash[:notice] = "権限がありません"
+        redirect_to("posts/index")
+      end
+    end
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_041358) do
+ActiveRecord::Schema.define(version: 2020_09_23_040403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_09_21_041358) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
